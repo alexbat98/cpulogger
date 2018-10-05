b1b036e5ebac615062db8c12c83e74e978bf9319
Alex <alexbatashev@gmail.com>
2018-10-05 15:44:35 +0300

    Initial Commit

---
+++ cpumeasure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpumeasure
+//
+//  Created by Александр Баташев on 05/10/2018.
+//  Copyright © 2018 Александр Баташев. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
